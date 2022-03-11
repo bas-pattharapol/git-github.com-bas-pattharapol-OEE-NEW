292879203f2fb1f714ee2d7a2dd735445015625f
Pattharapol Choonarong <baskzxcpatt@gmail.com>
2022-03-11 17:04:57 +0700

    master

--- web/app.py
+++ web/app.py
@@ -328,6 +328,7 @@ def API_RunTime_DownTime():
         print(startDate)
         print(endDate)
         print(data['DonwTime'][i]['Post_Date'])
+        
         cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
         update = cnxn.cursor()
         update.execute('INSERT INTO OEE_DB.dbo.INF_OEE2_V2 (PDOrder, TypeTime, Operation, PostDate, StartTime, EndTime, [Min],DownTimeCode) VALUES(?,?,?,?,?,?,?,?)' ,(data['Order'],"DonwTime",data['Operation'],data['DonwTime'][i]['Post_Date'],startDate,endDate,data['DonwTime'][i]['Total_Downtime'],data['DonwTime'][i]['Reason_Var']))
